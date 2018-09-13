12fae31c09ab3ee8e491935ab1ba9c32234de153
Krendelev <krendelev@gmail.com>
2018-09-13 06:35:19 +0300

    implement main features

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+This script will calculate the strength of your password according to the following criteria:
 
-# Project Goals
+- minimum length of 8 characters
+- lack of character repetition
+- the use of both upper-case and lower-case letters (case sensitivity)
+- inclusion of one or more numerical digits
+- inclusion of special characters, such as @, #, $
+
+In addition, it can check your password against passwords list if provided. Such list should be in `txt` format.
+
+## Quick Start
+
+```bash
+$ python password_strength.py kD4NqEBCCPFC
+Your passwords strength is 7 out of 10
+
+$ python password_strength.py abc123 worst.txt
+Your passwords strength is 1 out of 10
+```
+
+## Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,63 @@
+import string
+import argparse
 
 
-def get_password_strength(password):
-    pass
+CHECK_WEIGHT = 2
+
+
+def check_case(password, case):
+    return not set(password).isdisjoint(set(case))
+
+
+def check_letters(password):
+    strings = {
+        string.ascii_lowercase,
+        string.ascii_uppercase,
+        string.digits,
+        string.punctuation,
+    }
+    return sum(
+        CHECK_WEIGHT * check_case(password, string)
+        for string in strings
+        )
+
+
+def check_length(password):
+    min_length = 8
+    return CHECK_WEIGHT * (len(password) >= min_length)
+
+
+def check_repetitions(password):
+    return len(password) > len(set(password))
+
+
+def check_list(password, password_list):
+    try:
+        with open(password_list) as list_handler:
+            return int(password in list_handler.read())
+    except FileNotFoundError:
+        print('Password list not found')
+
+
+def get_password_strength(password, password_list=None):
+    return (
+        check_list(password, password_list) if password_list else None
+        ) or (
+        check_letters(password) +
+        check_length(password) -
+        check_repetitions(password)
+        )
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('password')
+    parser.add_argument('password_list', nargs='?')
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    pass
+    args = get_args()
+    print('Your passwords strength is {} out of 10'.format(
+        get_password_strength(args.password, args.password_list)
+        ))
